9602982b28bd33852d21042f16b64a53f80d07be
tarun <tarunnarang1992@gmail.com>
2019-03-01 08:18:20 +0530

    ok

--- Section 5 - Working With ExpressJS/app.js
+++ Section 5 - Working With ExpressJS/app.js
@@ -1,7 +1,28 @@
 const http = require('http');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded({extended: true}));
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+    
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello Express!</h1>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
